e9413197c235fd215c605f97510b6bff9a251f6b
im_TaeHo_K <xogh8025@naver.com>
2022-01-30 15:11:28 +0900

    refactor Make percent change object for reduce variation

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -36,6 +36,18 @@ const PriceItem = styled.div<PercentProps>`
       props.percent ? (props?.percent > 0 ? "red" : "blue") : null};
   }
 `;
+const UpDown = (percent: number | undefined) => {
+  if (percent) {
+    if (percent > 0) {
+      return "▲ " + percent;
+    } else if (percent) {
+      const abs = Math.abs(percent);
+      return "▼ " + abs;
+    }
+    return;
+  }
+  return;
+};
 
 function Price({ coinId }: PriceProps) {
   const { isLoading, data } = useQuery<PriceData>(
@@ -45,23 +57,13 @@ function Price({ coinId }: PriceProps) {
       refetchInterval: 5000,
     }
   );
-  const halfMin = data?.quotes.USD.percent_change_30m;
-  const oneHour = data?.quotes.USD.percent_change_1h;
-  const twlvHour = data?.quotes.USD.percent_change_12h;
-  const oneDay = data?.quotes.USD.percent_change_24h;
-
-  const UpDown = (percent: number | undefined) => {
-    if (percent) {
-      if (percent > 0) {
-        return "▲ " + percent;
-      } else if (percent) {
-        const abs = Math.abs(percent);
-        return "▼ " + abs;
-      }
-      return;
-    }
-    return;
+  const percentChange = {
+    halfMin: data?.quotes.USD.percent_change_30m,
+    oneHour: data?.quotes.USD.percent_change_1h,
+    twlvHour: data?.quotes.USD.percent_change_12h,
+    oneDay: data?.quotes.USD.percent_change_24h,
   };
+
   return (
     <>
       {isLoading ? (
@@ -72,21 +74,21 @@ function Price({ coinId }: PriceProps) {
             <span>Price</span>
             <span>$ {data?.quotes.USD.price.toFixed(2)} </span>
           </PriceItem>
-          <PriceItem percent={halfMin}>
+          <PriceItem percent={percentChange.halfMin}>
             <span>Change rate(30m)</span>
-            <span>{UpDown(halfMin)}</span>
+            <span>{UpDown(percentChange.halfMin)}</span>
           </PriceItem>
-          <PriceItem percent={oneHour}>
+          <PriceItem percent={percentChange.oneHour}>
             <span>Change rate(1h)</span>
-            <span>{UpDown(oneHour)}</span>
+            <span>{UpDown(percentChange.oneHour)}</span>
           </PriceItem>
-          <PriceItem percent={twlvHour}>
+          <PriceItem percent={percentChange.twlvHour}>
             <span>Change rate(12h)</span>
-            <span>{UpDown(twlvHour)}</span>
+            <span>{UpDown(percentChange.twlvHour)}</span>
           </PriceItem>
-          <PriceItem percent={oneDay}>
+          <PriceItem percent={percentChange.oneDay}>
             <span>Change rate(1d)</span>
-            <span>{UpDown(oneDay)}</span>
+            <span>{UpDown(percentChange.oneDay)}</span>
           </PriceItem>
         </PriceContainer>
       )}
